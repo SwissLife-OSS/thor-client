aa6b7fec23dc70b2b3c7b3bed6b278aea65db4ba
Rafael Staib <me@rafaelstaib.com>
2017-09-12 00:17:02 +0200

    Added further tests

---
+++ src/Tests/ApplicationEventSourceTests.cs
@@ -0,0 +1,22 @@
+﻿using ChilliCream.Tracing.Analyzer;
+using FluentAssertions;
+using Xunit;
+
+namespace ChilliCream.Tracing.Tests
+{
+    public class ApplicationEventSourceTests
+    {
+        [Fact(DisplayName = "Analyze: Inspects ApplicationEventSource schema")]
+        public void Analyze()
+        {
+            // Arrange
+            EventSourceAnalyzer analyzer = new EventSourceAnalyzer();
+
+            // Act
+            Report report = analyzer.Inspect(ApplicationEventSource.Log);
+
+            // Assert
+            report.HasErrors.Should().BeFalse();
+        }
+    }
+}
