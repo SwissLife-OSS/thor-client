d5217a06b6faa7729f3b90e3fc6e700b87c61b09
Rafael Staib <me@rafaelstaib.com>
2017-09-12 23:48:20 +0200

    Disabled TravisCI and removed its badge from the readme (#4)

---
+++ src/Tests/ActivityEventSourceTests.cs
@@ -0,0 +1,22 @@
+﻿using ChilliCream.Tracing.Analyzer;
+using FluentAssertions;
+using Xunit;
+
+namespace ChilliCream.Tracing.Tests
+{
+    public class ActivityEventSourceTests
+    {
+        [Fact(DisplayName = "Analyze: Inspects ApplicationEventSource schema")]
+        public void Analyze()
+        {
+            // Arrange
+            EventSourceAnalyzer analyzer = new EventSourceAnalyzer();
+
+            // Act
+            Report report = analyzer.Inspect(ActivityEventSource.Log);
+
+            // Assert
+            report.HasErrors.Should().BeFalse();
+        }
+    }
+}
