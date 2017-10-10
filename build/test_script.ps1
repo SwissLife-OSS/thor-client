0ae2ef0f68d8ea9ca588d0278b6cb6a82442c189
Rafael Staib <me@rafaelstaib.com>
2017-10-10 23:35:16 +0200

    Fixed code coverage issue

--- build/test_script.ps1
+++ build/test_script.ps1
@@ -6,7 +6,7 @@ if (Test-Path env:APPVEYOR)
     }
     else
     {
-        Invoke-Expression ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe -register:user -target:"build\runtests.bat" -searchdirs:".\src\*\bin\' + $env:CONFIGURATION + '\netcoreapp2.0" -oldstyle -output:coverage.xml -skipautoprops -returntargetcode -filter:"+[*Analyzer]*"')
+        Invoke-Expression ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe -register:user -target:"build\runtests.bat" -searchdirs:".\src\*\bin\Debug\netstandard2.0" -oldstyle -output:coverage.xml -skipautoprops -returntargetcode -filter:"+[*Tracing]*"')
         Invoke-Expression ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe --opencover coverage.xml')
         dotnet pack .\src\Tracing.sln --include-symbols --include-source -c $env:CONFIGURATION /p:PackageVersion=$env:CC_BUILD_VERSION
     }
