67a65152d6ed7bc605abf8514fb77b55dee234a9
Rafael Staib <me@rafaelstaib.com>
2017-10-10 21:46:51 +0200

    Added missing choco install statement for sonar

--- build/build_script.ps1
+++ build/build_script.ps1
@@ -4,8 +4,8 @@ if (Test-Path env:APPVEYOR)
     dotnet restore .\src\Tracing.sln
 
     # Build & Analyse
-    # Sonar for .Net Core is not supported right now; for more information go to https://jira.sonarsource.com/browse/SONARMSBRU-310
-    Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"' + $env:APPVEYOR_PROJECT_NAME + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:CC_BUILD_VERSION + '"')
+    choco install "msbuild-sonarqube-runner" -y
+    Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /n:"' + $env:APPVEYOR_PROJECT_NAME + '" /k:"' + $env:SONARQUBE_PROJECT_KEY + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:CC_BUILD_VERSION + '"')
     msbuild .\src\Tracing.sln /p:Configuration=$env:CONFIGURATION /p:Version=$env:CC_BUILD_VERSION
     Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
 }
