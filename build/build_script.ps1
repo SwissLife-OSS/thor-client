7be328675b5d93302d70164ce03cb3ba53e35668
Rafael Staib <me@rafaelstaib.com>
2017-10-10 21:14:34 +0200

    Enabled sonar

--- build/build_script.ps1
+++ build/build_script.ps1
@@ -5,7 +5,7 @@ if (Test-Path env:APPVEYOR)
 
     # Build & Analyse
     # Sonar for .Net Core is not supported right now; for more information go to https://jira.sonarsource.com/browse/SONARMSBRU-310
-    #Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"' + $env:APPVEYOR_PROJECT_NAME + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:CC_BUILD_VERSION + '"')
-    dotnet build .\src\Tracing.sln -c $env:CONFIGURATION /p:Version=$env:CC_BUILD_VERSION
-    #Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
+    Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"' + $env:APPVEYOR_PROJECT_NAME + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:CC_BUILD_VERSION + '"')
+    msbuild .\src\Tracing.sln /p:Configuration=$env:CONFIGURATION /p:Version=$env:CC_BUILD_VERSION
+    Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
 }
