15b7390b3f2f09bcb7876c0cbd91a14ff74e6ca8
Rafael Staib <me@rafaelstaib.com>
2018-04-20 10:13:43 +0200

    Added interfaces for ETW event session and ETW event transmission #24

---
+++ src/Core/ITelemetryTransmitter.cs
@@ -0,0 +1,14 @@
+﻿namespace Thor.Core
+{
+    /// <summary>
+    /// A transmitter for telemetry events.
+    /// </summary>
+    public interface ITelemetryTransmitter
+    {
+        /// <summary>
+        /// Enqueues a single telemetry event for transmission.
+        /// </summary>
+        /// <param name="telemetryEvent">A telemetry event.</param>
+        void Enqueue(TelemetryEvent telemetryEvent);
+    }
+}
