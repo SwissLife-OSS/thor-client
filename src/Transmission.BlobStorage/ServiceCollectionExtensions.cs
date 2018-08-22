cce5c921d730e28fe521fd978b4e5df594383406
Rafael Staib <me@rafaelstaib.com>
2018-08-22 14:18:07 +0200

    Fixed a sonar bug

--- src/Transmission.BlobStorage/ServiceCollectionExtensions.cs
+++ src/Transmission.BlobStorage/ServiceCollectionExtensions.cs
@@ -38,7 +38,7 @@ namespace Thor.Core.Transmission.BlobStorage
                 .AddSingleton(p =>
                 {
                     BlobStorageConfiguration config = p.GetRequiredService<IOptions<BlobStorageConfiguration>>()?.Value;
-                    CloudStorageAccount account = CloudStorageAccount.Parse(config.ConnectionString);
+                    CloudStorageAccount account = CloudStorageAccount.Parse(config?.ConnectionString);
                     CloudBlobClient client = account.CreateCloudBlobClient();
                     CloudBlobContainer container = client.GetContainerReference(config.AttachmentContainerName);
 
