2d0ae5033a10f947743d88b9b5ee1071b73e690d
Rafael Staib <me@rafaelstaib.com>
2018-08-22 14:36:19 +0200

    Fixed another sonar bug

--- src/Transmission.BlobStorage/ServiceCollectionExtensions.cs
+++ src/Transmission.BlobStorage/ServiceCollectionExtensions.cs
@@ -40,7 +40,7 @@ namespace Thor.Core.Transmission.BlobStorage
                     BlobStorageConfiguration config = p.GetRequiredService<IOptions<BlobStorageConfiguration>>()?.Value;
                     CloudStorageAccount account = CloudStorageAccount.Parse(config?.ConnectionString);
                     CloudBlobClient client = account.CreateCloudBlobClient();
-                    CloudBlobContainer container = client.GetContainerReference(config.AttachmentContainerName);
+                    CloudBlobContainer container = client.GetContainerReference(config?.AttachmentContainerName);
 
                     container.CreateIfNotExistsAsync().GetAwaiter().GetResult();
 
@@ -52,7 +52,7 @@ namespace Thor.Core.Transmission.BlobStorage
                 {
                     TracingConfiguration config = p.GetRequiredService<IOptions<TracingConfiguration>>()?.Value;
 
-                    return new BlobStorageTransmissionStorage(config.GetAttachmentsStoragePath());
+                    return new BlobStorageTransmissionStorage(config?.GetAttachmentsStoragePath());
                 })
                 .AddSingleton<ITelemetryAttachmentTransmitter, BlobStorageTransmitter>();
         }
