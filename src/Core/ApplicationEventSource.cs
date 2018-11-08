b8eaad3c80a62288077665c4842efd4dc975ba92
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2018-11-08 18:28:37 +0100

    Add UnhandledException event on Application (#50)

--- src/Core/ApplicationEventSource.cs
+++ src/Core/ApplicationEventSource.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics.Tracing;
+﻿using System;
+using System.Diagnostics.Tracing;
 using Thor.Core.Abstractions;
+using Thor.Core.Transmission.Abstractions;
 
 namespace Thor.Core
 {
@@ -9,6 +11,7 @@ namespace Thor.Core
     {
         private const int _startEventId = 1;
         private const int _stopEventId = 2;
+        private const int _unhandledEventId = 3;
 
         public static ApplicationEventSource Log { get; } = new ApplicationEventSource();
 
@@ -23,6 +26,51 @@ namespace Thor.Core
             Task = Tasks.Application, Opcode = EventOpcode.Stop)]
         public void Stop(int applicationId) =>
             WriteEvent(_stopEventId, applicationId);
+        
+        [NonEvent]
+        public void UnhandledExceptionOccurred(Exception exception)
+        {
+            if (IsEnabled())
+            {
+                AttachmentId attachmentId = AttachmentId.NewId();
+                ExceptionAttachment attachment = AttachmentFactory.Create(attachmentId,
+                    nameof(exception), exception);
+
+                AttachmentDispatcher.Instance.Dispatch(attachment);
+                UnhandledExceptionOccurred(Application.Id, ActivityStack.Id, attachmentId);
+            }
+        }
+
+        [Event(_unhandledEventId, Level = EventLevel.Error,
+            Message = "Unhandled exception occurred.", Version = 1)]
+        private void UnhandledExceptionOccurred(
+            int applicationId, Guid activityId, string attachmentId)
+        {
+            UnhandledExceptionOccurredCore(
+                _unhandledEventId, applicationId, activityId, attachmentId);
+        }
+
+        [NonEvent]
+        private unsafe void UnhandledExceptionOccurredCore(
+            int eventId, int applicationId, Guid activityId, string attachmentId)
+        {
+            StringExtensions.SetToEmptyIfNull(ref attachmentId);
+
+            fixed (char* attachmentIdBytes = attachmentId)
+            {
+                const short dataCount = 3;
+                EventData* data = stackalloc EventData[dataCount];
+
+                data[0].DataPointer = (IntPtr)(&applicationId);
+                data[0].Size = 4;
+                data[1].DataPointer = (IntPtr)(&activityId);
+                data[1].Size = 16;
+                data[2].DataPointer = (IntPtr)attachmentIdBytes;
+                data[2].Size = ((attachmentId.Length + 1) * 2);
+
+                WriteEventCore(eventId, dataCount, data);
+            }
+        }
 
         #region Tasks and Opcodes
 
