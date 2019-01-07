3cd69907c0f43bb110f429b0f07b9909713cfb19
glucaci <gabriel.lucaci.dev@gmail.com>
2019-01-07 14:48:15 +0100

    Move IDiagnosticLister and DiagnosticListerInitializer from AspNetCore to Core

---
+++ src/Core.Abstractions/IDiagnosticsListener.cs
@@ -0,0 +1,13 @@
+﻿namespace Thor.Core.Abstractions
+{
+    /// <summary>
+    /// Describe a contract for a diagnostic source listener
+    /// </summary>
+    public interface IDiagnosticsListener
+    {
+        /// <summary>
+        /// Name of the diagnostic listener
+        /// </summary>
+        string Name { get; }
+    }
+}
