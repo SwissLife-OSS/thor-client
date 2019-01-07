ec8e29d5738e4e46704af07ccc2db8ec14ebce34
glucaci <gabriel.lucaci.dev@gmail.com>
2019-01-07 16:54:56 +0100

    Tests (WIP) for HotChocolateDiagnosticListener

---
+++ src/libraries/HotChocolate.Tests/HotChocolateDiagnosticListenerTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HotChocolate.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace Thor.HotChocolate.Tests
+{
+    public class HotChocolateDiagnosticListenerTests
+        : IClassFixture<TestServerFactory>
+    {
+        public HotChocolateDiagnosticListenerTests(TestServerFactory testServerFactory)
+        {
+            TestServerFactory = testServerFactory;
+        }
+
+        private TestServerFactory TestServerFactory { get; set; }
+        private HttpContext Context { get; set; }
+
+        [Fact]
+        public async Task HotChocolateActivity_Exist()
+        {
+            // arrange
+            TestServer server = CreateTestServer();
+            var request = new
+            {
+                Query = @"{ customProperty }"
+            };
+
+            // act
+            HttpResponseMessage message =
+                await server.SendRequestAsync(request);
+
+            Assert.Equal(HttpStatusCode.OK, message.StatusCode);
+
+            // TODO: HttpContext check activity
+        }
+
+        private TestServer CreateTestServer(string path = null)
+        {
+            return TestServerFactory.Create(
+                new QueryMiddlewareOptions
+                {
+                    Path = path ?? "/",
+                    OnCreateRequest = (context, request, properties, ct) =>
+                    {
+                        Context = context;
+                        properties["foo"] = "bar";
+                        return Task.CompletedTask;
+                    }
+                });
+        }
+
+    }
+}
---
+++ src/libraries/HotChocolate.Tests/TestServerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+
+namespace Thor.HotChocolate.Tests
+{
+    internal static class TestServerExtensions
+    {
+        public static Task<HttpResponseMessage> SendRequestAsync<TObject>(
+            this TestServer testServer, TObject requestBody, string path = null)
+        {
+            var httpContent = new StringContent(
+                JsonConvert.SerializeObject(requestBody),
+                Encoding.UTF8,
+                "application/json");
+
+            return testServer.CreateClient()
+                .PostAsync(path ?? "/",
+                    httpContent);
+        }
+    }
+}
