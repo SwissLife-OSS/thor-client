62a48448c253dbb1b1b03166c0fcc86cfde384ec
glucaci <gabriel.lucaci.dev@gmail.com>
2019-10-17 11:46:39 +0200

    remove targeting net47 and net461

--- src/Core/Session.Abstractions/ProbeEventListener.cs
+++ src/Core/Session.Abstractions/ProbeEventListener.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 
-#if NET461
-using System;
-using System.Collections.ObjectModel;
-#endif
-
 namespace Thor.Core.Session.Abstractions
 {
     /// <summary>
@@ -33,49 +28,5 @@ namespace Thor.Core.Session.Abstractions
                 _queue.Enqueue(eventData);
             }
         }
-
-        #if NET461
-
-        /// <summary>
-        /// An event whhich is fired when an eventsource is successfully added to the listeners.
-        /// </summary>
-        public event EventHandler<EventSourceCreatedEventArgs> EventSourceCreated;
-
-        /// <inheritdoc/>
-        protected override void OnEventSourceCreated(EventSource eventSource)
-        {
-            EventSourceCreated?.Invoke(this, new EventSourceCreatedEventArgs(eventSource));
-        }
-
-        #endif
     }
-
-    #if NET461
-
-    /// <summary>
-    /// Provides data for the System.Diagnostics.Tracing.EventListener.EventSourceCreated event.
-    /// </summary>
-    public class EventSourceCreatedEventArgs
-        : EventArgs
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EventSourceCreatedEventArgs"/> class.
-        /// </summary>
-        public EventSourceCreatedEventArgs(EventSource eventSource)
-        {
-            if (eventSource == null)
-            {
-                throw new ArgumentNullException(nameof(eventSource));
-            }
-
-            EventSource = eventSource;
-        }
-
-        /// <summary>
-        /// Get the event source that is attaching to the listener.
-        /// </summary>
-        public EventSource EventSource { get; }
-    }
-
-    #endif
 }
