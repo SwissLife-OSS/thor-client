0a47d2ce077a7edb9a83744d2683df6d1009a830
glucaci <gabriel.lucaci.dev@gmail.com>
2020-06-04 10:07:00 +0200

    Fix namespace

--- src/Core/Transmission.Abstractions/Job.cs
+++ src/Core/Transmission.Abstractions/Job.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Thor.Core.Abstractions
+namespace Thor.Core.Transmission.Abstractions
 {
     internal class Job : IDisposable
     {
